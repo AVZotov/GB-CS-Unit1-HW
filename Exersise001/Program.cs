e5fd382d9467a26ee8ac594fd8fe7b625026b9aa
Alexey_Zotov <Alexey.Zotov@ymail.com>
2023-04-08 00:13:22 +0300

    Exersise001 added

---
+++ Exersise001/Program.cs
@@ -0,0 +1,17 @@
+﻿int numberA;
+int numberB;
+
+Console.Clear();
+Console.WriteLine("Program to determine max from 2 numbers");
+Console.Write("Enter first number: ");
+numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter second number: ");
+numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > numberB)
+    Console.WriteLine($"max = {numberA}");
+else if (numberB > numberA)
+    Console.WriteLine($"max = {numberB}");
+else
+    Console.WriteLine("values are equal");
+
