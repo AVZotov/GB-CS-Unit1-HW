09cb3c85e1dedc5f1f910f48f5b50d31f8cd5dd7
Alexey_Zotov <Alexey.Zotov@ymail.com>
2023-04-08 00:41:32 +0300

    exersise002 added

---
+++ Exersise002/Program.cs
@@ -0,0 +1,20 @@
+﻿int numberA, numberB, numberC, max;
+
+
+Console.Clear();
+Console.WriteLine("Program to determine max from 3 numbers");
+Console.Write("Enter first number: ");
+numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter second number: ");
+numberB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter third number: ");
+numberC = Convert.ToInt32(Console.ReadLine());
+max = numberA;
+
+if (numberB > max)
+    max = numberB;
+if  (numberC > max)
+    max = numberC;
+
+Console.WriteLine ($"max value: {max}");
+
