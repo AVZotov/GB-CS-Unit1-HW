7464060dddac5520e0dca385b563cf36f743684c
Alexey_Zotov <Alexey.Zotov@ymail.com>
2023-04-09 21:56:19 +0300

    Exersise003 project added

---
+++ Exersise003/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Program to determine if number if even or odd");
+Console.Write("Enter the number: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+if (userNumber % 2 == 0)
+{
+    Console.WriteLine($"{userNumber} is even");
+}
+else
+{
+    Console.WriteLine($"{userNumber} is odd");
+}
