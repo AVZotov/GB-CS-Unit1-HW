6326e58a78f29a4f83a66a64bd328fc85d7ea56d
Alexey_Zotov <Alexey.Zotov@ymail.com>
2023-04-09 22:12:32 +0300

    Exersise004 project added

---
+++ Exersise004/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Program to print all even values from 1 to N");
+Console.Write("Enter the value (N): ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+int counter = 1;
+
+while (counter <= userNumber)
+{
+    if (counter % 2 == 0)
+    {
+        Console.Write($"{counter}, ");
+    }
+
+    counter ++;
+}
